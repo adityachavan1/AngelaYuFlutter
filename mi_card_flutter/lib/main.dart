a97f41f8e27918c42055e8a7a0ae0b211e7007b8
Aditya <atuladityasingh001@gmail.com>
2021-05-28 05:52:53 +0530

    Completed mi_card app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.teal[100],
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1506803682981-6e718a9dd3ee?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=c3a31eeb7efb4d533647e3cad1de9257'),
+              ),
+              Text(
+                'Aditya Chauhan',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontFamily: 'SourceSansPro'),
+              ),
+              SizedBox(
+                  width: 150.0,
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  )),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Column(
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.phone,
+                            color: Colors.teal,
+                          ),
+                          title: Text(
+                            '+91 6203220590',
+                            style: TextStyle(
+                                color: Colors.teal.shade900,
+                                fontSize: 18.0,
+                                fontFamily: 'SourceSansPro'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(3.0),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.email,
+                            color: Colors.teal,
+                          ),
+                          title: Text(
+                            'atuladityasingh001@gmail.com',
+                            style: TextStyle(
+                                color: Colors.teal.shade900,
+                                fontSize: 18.0,
+                                fontFamily: 'SourceSansPro'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
